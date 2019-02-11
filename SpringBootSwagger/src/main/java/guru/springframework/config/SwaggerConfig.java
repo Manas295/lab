c1a113db84945aa5a7496752c0536b8d91a22e6c
Manas295 <manasbansal672@gmail.com>
2019-02-11 21:48:31 +0530

    Swagger chnages

--- SpringBootSwagger/src/main/java/guru/springframework/config/SwaggerConfig.java
+++ SpringBootSwagger/src/main/java/guru/springframework/config/SwaggerConfig.java
@@ -1,17 +1,18 @@
 package guru.springframework.config;
 
+import static springfox.documentation.builders.PathSelectors.regex;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
-import springfox.documentation.builders.ApiInfoBuilder;
+
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
-import static springfox.documentation.builders.PathSelectors.regex;
 
 @Configuration
 @EnableSwagger2
@@ -20,10 +21,10 @@ public class SwaggerConfig extends WebMvcConfigurationSupport {
     public Docket productApi() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("guru.springframework.controllers"))
+                .apis(RequestHandlerSelectors.basePackage("guru.springframework"))
                 .paths(regex("/product.*"))
-                .build()
-                .apiInfo(metaData());
+                .build();
+               // .apiInfo(metaData());
     }
 	/*
 	 * private ApiInfo metaData() { return new ApiInfoBuilder()
@@ -34,27 +35,22 @@ public class SwaggerConfig extends WebMvcConfigurationSupport {
 	 * Contact("Manas Bansal", "https://springframework.guru/about/",
 	 * "manasbansal672@gmail.com")) .build(); }
 	 */
-    private ApiInfo metaData() {
-
-        ApiInfo apiInfo = new ApiInfo(
-                "Spring Boot Swagger Example API",
-                "Spring Boot Swagger Example API for Youtube",
-                "1.0",
-                "Terms of Service",
-                new Contact("TechPrimers", "https://www.youtube.com/TechPrimers",
-                        "techprimerschannel@gmail.com"),
-                "Apache License Version 2.0",
-                "https://www.apache.org/licesen.html"
-        );
-
-        return apiInfo;
-    }
-    
-	
-	  @Override protected void addResourceHandlers(ResourceHandlerRegistry
-	  registry) { registry.addResourceHandler("swagger-ui.html")
-	  .addResourceLocations("classpath:/META-INF/resources/");
-	  
-	 registry.addResourceHandler("/webjars/**")
-	  .addResourceLocations("classpath:/META-INF/resources/webjars/"); } 
-}
+	/*
+	 * private ApiInfo metaData() {
+	 * 
+	 * ApiInfo apiInfo = new ApiInfo( "Spring Boot Swagger Example API",
+	 * "Spring Boot Swagger Example API for Youtube", "1.0", "Terms of Service", new
+	 * Contact("TechPrimers", "https://www.youtube.com/TechPrimers",
+	 * "techprimerschannel@gmail.com"), "Apache License Version 2.0",
+	 * "https://www.apache.org/licesen.html" );
+	 * 
+	 * return apiInfo; }
+	 * 
+	 * 
+	 * @Override protected void addResourceHandlers(ResourceHandlerRegistry
+	 * registry) { registry.addResourceHandler("swagger-ui.html")
+	 * .addResourceLocations("classpath:/META-INF/resources/");
+	 * 
+	 * registry.addResourceHandler("/webjars/**")
+	 * .addResourceLocations("classpath:/META-INF/resources/webjars/"); }
+	 */}
