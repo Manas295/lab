7bf23fcff62ff582404e97d9629f249ab571f116
Manas295 <manasbansal672@gmail.com>
2019-02-11 23:26:15 +0530

    Pending Files

---
+++ SpringBootSwaggerDocumentation/SpringBootSwaggerDocumentation/src/main/java/com/swagger/springboot/SpringBootSwaggerDocumentation/SpringBootSwaggerDocumentationApplication.java
@@ -0,0 +1,14 @@
+package com.swagger.springboot.SpringBootSwaggerDocumentation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSwaggerDocumentationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSwaggerDocumentationApplication.class, args);
+	}
+
+}
+
---
+++ SpringBootSwaggerDocumentation/SpringBootSwaggerDocumentation/src/test/java/com/swagger/springboot/SpringBootSwaggerDocumentation/SpringBootSwaggerDocumentationApplicationTests.java
@@ -0,0 +1,17 @@
+package com.swagger.springboot.SpringBootSwaggerDocumentation;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootSwaggerDocumentationApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
