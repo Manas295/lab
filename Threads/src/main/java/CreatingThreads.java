87cc0dbf3da831593f4ef2446a0238dd93ab217e
Manas295 <manasbansal672@gmail.com>
2019-07-14 00:14:15 +0530

    Threads with Lambda

--- Threads/src/main/java/CreatingThreads.java
+++ Threads/src/main/java/CreatingThreads.java
@@ -8,6 +8,9 @@ public class CreatingThreads {
 		ByImplementingRunnable byRunnable		= new ByImplementingRunnable();
 		Thread thread	= new Thread(byRunnable);
 		thread.start();
+		
+		ByLambdaExpressionThread  byLambdaExpressionThread =  new ByLambdaExpressionThread("Lambda Thread");
+		
 	}
 }
 
@@ -25,3 +28,20 @@ class ByImplementingRunnable implements Runnable{
 		System.out.println(this.getClass().getName());
 	}
 }
+
+class ByLambdaExpressionThread{
+	public ByLambdaExpressionThread(String name) {
+		// Lambda block - code inside the lambda block will
+        // be the implementation of the run method. 
+		Runnable r = () -> {
+            System.out.println("Entered run method " +Thread.currentThread().getName());
+            for(int i = 0; i < 5; i++){
+                System.out.println("Lambda thread " + i);
+            }
+            System.out.println("Finished with Lambda Thread");
+        };    
+        //starting thread with the constructor of the thread class
+        // that takes runnable instance and String as parameters
+        new Thread(r, name).start();
+		}
+	}
