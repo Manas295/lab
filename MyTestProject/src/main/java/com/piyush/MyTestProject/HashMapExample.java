6e12e656f1a789e6499f839f0145fd60dba5d7fb
Manas295 <manabansal672@gmail.com>
2019-07-25 10:18:57 +0530

    Hashmap Example added

--- MyTestProject/src/main/java/com/piyush/MyTestProject/HashMapExample.java
+++ MyTestProject/src/main/java/com/piyush/MyTestProject/HashMapExample.java
@@ -11,6 +11,14 @@ public class HashMapExample {
 		hm.put("3", "IT");
 		hm.put("4", "ME");
 		System.out.println("Val from Map "+hm.get("1"));
+		
+		String s = "manas";
+		String s1  = new String("manas");
+		
+		hm.put(s, "Hi");
+		hm.put(s1, "Hello");
+		System.out.println("hashcode s "+s.hashCode()+" s1 "+s1.hashCode());
+		System.out.println("Size --> "+hm.size());
 	}
 
 }
