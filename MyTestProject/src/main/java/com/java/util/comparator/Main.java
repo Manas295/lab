7cde5c9acc4595df309c008e9a800a4321f882a4
Manas295 <manabansal672@gmail.com>
2019-08-01 13:10:31 +0530

    OOPS Project Updated

--- MyTestProject/src/main/java/com/java/util/comparator/Main.java
+++ MyTestProject/src/main/java/com/java/util/comparator/Main.java
@@ -8,7 +8,7 @@ class Main
 {
 	public static void main(String[] args)
 	{
-		ArrayList<Movie> list = new ArrayList<Movie>();
+		List<Movie> list = new ArrayList<Movie>();
 		list.add(new Movie("Force Awakens", 8.3, 2015));
 		list.add(new Movie("Star Wars", 8.7, 1977));
 		list.add(new Movie("Empire Strikes Back", 8.8, 1980));
@@ -20,7 +20,7 @@ class Main
 		System.out.println("Sorted by rating");
 		RatingCompare ratingCompare = new RatingCompare();
 		// Collections.sort(list, ratingCompare);
-		Collections.sort(list, ratingCompare);
+		Collections.sort(list,Comparator.comparing(Movie::getRating));
 		for (Movie movie: list)
 			System.out.println(movie.getRating() + " " +
 					movie.getName() + " " +
@@ -31,7 +31,10 @@ class Main
 		// (Same three steps as above)
 		System.out.println("\nSorted by name");
 		NameCompare nameCompare = new NameCompare();
-		Collections.sort(list, nameCompare);
+	//	Collections.sort(list, nameCompare);
+		Collections.sort(list, (Movie m1, Movie m2) -> {
+			return m1.getName().compareTo(m2.getName());
+		});
 		for (Movie movie: list)
 			System.out.println(movie.getName() + " " +
 					movie.getRating() + " " +
