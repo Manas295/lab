50b07f37cf4435e145a8ece884e438b26851998f
Manas295 <manabansal672@gmail.com>
2019-08-13 13:03:59 +0530

    Association/Aggregation/Composition

---
+++ MyTestProject/src/main/java/com/association/Association.java
@@ -0,0 +1,51 @@
+package com.association;
+
+
+
+// Association between both the  
+// classes in main method 
+class Association  
+{ 
+	public static void main (String[] args)  
+	{ 
+		Bank bank = new Bank("Axis"); 
+		Employee emp = new Employee("Neha"); 
+
+		System.out.println(emp.getEmployeeName() +  
+				" is employee of " + bank.getBankName()); 
+	} 
+} 
+
+//employee class  
+class Employee 
+{ 
+	private String name; 
+
+	// employee name  
+	Employee(String name)  
+	{ 
+		this.name = name; 
+	} 
+
+	public String getEmployeeName() 
+	{ 
+		return this.name; 
+	}  
+} 
+
+
+class Bank  
+{ 
+	private String name; 
+
+	// bank name 
+	Bank(String name) 
+	{ 
+		this.name = name; 
+	} 
+
+	public String getBankName() 
+	{ 
+		return this.name; 
+	} 
+}  
