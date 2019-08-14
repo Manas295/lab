46de410e20d7395cd2391487bd322a20a09a29df
Manas295 <manabansal672@gmail.com>
2019-08-14 20:54:57 +0530

    hashmap sorting

--- HashCodeOrEquals/src/com/hashmapsort/HashMapSorting.java
+++ HashCodeOrEquals/src/com/hashmapsort/HashMapSorting.java
@@ -3,11 +3,13 @@ package com.hashmapsort;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Map;
-import java.util.stream.Collectors;
+import  java.util.stream.Collectors;
+
+
 
 public class HashMapSorting {
 
-    public static void main(String[] args) {
+	public static void main(String[] args) {
         Map<String,String> langMap = new HashMap<String,String>();
         langMap.put("ENG", "English");
         langMap.put("NLD", "Dutch");
@@ -20,12 +22,17 @@ public class HashMapSorting {
                           " Value- " + lang.getValue());
         }
         Map<String,String> newMap = langMap.entrySet().stream().sorted(Map.Entry.comparingByValue())
-                                    .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (k,v)->k,LinkedHashMap::new));
-        
-        Map<String,String> newMap1 = langMap.entrySet().stream().sorted(Map.Entry.comparingByValue())
+                                   .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (k,v)->k,LinkedHashMap::new));
+     
+
+        Map<String,String> newMap1 = langMap.entrySet().stream().sorted(HashMapSorting::comparingByValue)
         								.collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue,(k,v)->k,LinkedHashMap::new));
-        								
+
         System.out.println("-- Sorted Map --");
         newMap1.entrySet().forEach((e)->{System.out.println("Key- " + e.getKey() + " Value- " + e.getValue());});
     }
+
+	private static int comparingByValue(Map.Entry<String, String> entry1, Map.Entry<String, String> entry2) {
+		return entry2.getValue().compareTo(entry1.getValue());
+	}
 }
