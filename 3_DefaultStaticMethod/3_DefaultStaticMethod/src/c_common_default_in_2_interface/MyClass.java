6baf80c042a10a139b21b81a7fdc2792fc244815
Manas295 <manabansal672@gmail.com>
2019-08-22 22:42:37 +0530

    Defaukt method points added

--- 3_DefaultStaticMethod/3_DefaultStaticMethod/src/c_common_default_in_2_interface/MyClass.java
+++ 3_DefaultStaticMethod/3_DefaultStaticMethod/src/c_common_default_in_2_interface/MyClass.java
@@ -19,6 +19,22 @@ package c_common_default_in_2_interface;
 	         
 	    }
 	    
+/*
+ * 1 ) If implementing class overrides the default method and provides its own functionality for the default 
+method then the method of the class takes priority over the interface default methods.
+As Example - If MyClass provides its own implementation of displayGreeting(), 
+then the overridden method will be called not the default method in interface A or B.
+
+2 ) When class implements both interfaces and both have the same default method, also the class is 
+not overriding that method then the error will be thrown.
+"Duplicate default methods named displayGreeting inherited from the interfaces"
+
+3 ) In case when an interface extends another interface and both have the same default method, 
+the inheriting interface default method will take precedence. Thus, if interface B extends interface A 
+then the default method of interface B will take precedence.
+
+ */
+	    
 	  
 	 
 }
