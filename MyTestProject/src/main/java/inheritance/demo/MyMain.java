97573283da36f9b6d17ff5eb3160fbd50b3fe50d
Manas295 <manasbansal672@gmail.com>
2019-12-04 20:33:28 +0530

    Inheritnce updated

--- MyTestProject/src/main/java/inheritance/demo/MyMain.java
+++ MyTestProject/src/main/java/inheritance/demo/MyMain.java
@@ -13,27 +13,32 @@ class SuperClass{
 	
 	public SuperClass() {
 		a= 11;
+		System.out.println("Super Constructor "+a);
 	}
 	
 	public void display() {
 		a= 8;
 		System.out.println(a);
 	}
-
+	{
+		System.out.println("Super Instance Block "+a);
+	}
 }
 
 class ChildClass extends SuperClass{
 
 	//int a = 3;
-	
 	{
-		System.out.println(a);
+		System.out.println("Child Instance Block "+a);
 	}
 	
+	static{
+		System.out.println("static block in child");
+	}
 	
 	public ChildClass(){
 		a = 1;
-		System.out.println(a);
+		System.out.println("Child Constructor "+a);
 	}
 	
 	public void display() {
